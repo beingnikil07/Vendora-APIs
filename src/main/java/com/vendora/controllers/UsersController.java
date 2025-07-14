c7316d5cb4fb71c33693ad103a45b1b64de67ef0
nikhil <kumarnikil220>
2025-07-13 22:22:56 -0700

    users apis created for the vendora app

--- src/main/java/com/vendora/controllers/UsersController.java
+++ src/main/java/com/vendora/controllers/UsersController.java
@@ -1,10 +1,10 @@
 package com.vendora.controllers;
 import com.vendora.models.User;
 import com.vendora.services.UserServices;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -19,6 +19,28 @@ public class UsersController {
     @PostMapping("/create")
     public User createUser(@RequestBody User user){
         return userServices.createUser(user);
+    }
+
+    @PatchMapping("/update/{user_id}")
+    public ResponseEntity<User> updateUserDetails(@PathVariable Integer user_id, @RequestBody User user){
+        return userServices.updateUser(user_id,user);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable Integer id){
+        return userServices.deleteUser(id);
+    }
+
+    //To get a Single User Details
+    @GetMapping("/getuser/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Integer id){
+        return userServices.getUserById(id);
+    }
+
+    //To get all Users
+    @GetMapping("/getusers")
+    public ResponseEntity<List<User>> getAllUsers(){
+        return userServices.getAllUsers();
     }
 
 }
--- src/main/java/com/vendora/services/UserServicesImpl.java
+++ src/main/java/com/vendora/services/UserServicesImpl.java
@@ -2,7 +2,11 @@ package com.vendora.services;
 
 import com.vendora.models.User;
 import com.vendora.repository.UserRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserServicesImpl implements UserServices{
@@ -12,10 +16,72 @@ public class UserServicesImpl implements UserServices{
             this.userRepository = userRepository;
         }
 
+        //To create user
+        @Override
         public User createUser(User user){
-                User u=userRepository.save(user);
-                return u;
+                if(user!=null) {
+                        User u = userRepository.save(user);
+                        return u;
+                }else{
+                        return null;
+                }
         }
 
+        @Override
+        public ResponseEntity<User> updateUser(Integer user_id, User user) {
+                //find user
+                User existedUser=userRepository.findById(user_id).orElse(null);
+
+                if(existedUser==null){
+                        return ResponseEntity.notFound().build();
+                }
+
+                //update only fields that are coming in request
+                if(user.getName()!=null){
+                        existedUser.setName(user.getName());
+                }
+
+                if(user.getAddress()!=null){
+                        existedUser.setAddress(user.getAddress());
+
+                }
+                if(user.getEmail()!=null){
+                        existedUser.setEmail(user.getEmail());
+                }
+                if (user.getPassword()!=null){
+                        existedUser.setPassword(user.getPassword());
+                }
+                if(user.getPhone_no()!=null) {
+                        existedUser.setPhone_no(user.getPhone_no());
+                }
+                //save and return the existed user
+                User updatedUser = userRepository.save(existedUser);
+                return ResponseEntity.ok(updatedUser);
+        }
+
+
+        //To delete user --Admin accessible
+        public ResponseEntity<String>   deleteUser(Integer user_id){
+                if (!userRepository.existsById(user_id)){
+                     return new ResponseEntity<>("User not found",HttpStatus.NOT_FOUND);
+                }
+
+                userRepository.deleteById(user_id);
+                return ResponseEntity.ok("User deleted successfully");
+        }
+
+        //Get one user
+        @Override
+        public ResponseEntity<User> getUserById(Integer user_id) {
+                if(!userRepository.existsById(user_id)){
+                    return ResponseEntity.notFound().build();
+                }
+                return  ResponseEntity.ok(userRepository.findById(user_id).get());
+        }
+
+        @Override
+        public ResponseEntity<List<User>> getAllUsers() {
+                return ResponseEntity.ok(userRepository.findAll());
+        }
 
 }
